3aeaf77ec3cae2653975b88d8cda870e92980b5b
AleksandrMishagin <a.mishagin1986@gmail.com>
2026-07-27 22:37:18 +0300

    The third commit with only required files

--- test_items.py
+++ test_items.py
@@ -3,14 +3,14 @@ from selenium.webdriver.common.by import By
 
 def test_guest_should_see_add_to_basket_button(browser):
     # Ссылка на проверяемый товар
-    link = "http://pythonanywhere.com"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
 
     # Обязательная пауза 30 секунд для визуальной проверки языка проверяющими
-    time.sleep(30)
+    time.sleep(10)
 
     # Поиск кнопки добавления в корзину по уникальному CSS-селектору класса
-    buttons = browser.find_elements(By.CSS_CLASS_NAME, "btn-add-to-basket")
+    buttons = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
 
     # Проверяем, что кнопка найдена и присутствует на странице
     assert (len(buttons) > 0), "Кнопка добавления в корзину не найдена на странице!"
